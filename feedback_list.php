e20b6cee0553385df428ae065d4cc5617b37d929
shanthana06 <shanthanalaxhimi@gmail.com>
2025-08-24 13:23:54 +0800

    Delete feedback_list.php

--- feedback_list.php
+++
@@ -1,8 +0,0 @@
-<?php include 'db.php'; ?>
-<h2>Customer Feedback</h2>
-<?php
-$result = $conn->query("SELECT * FROM feedback ORDER BY submitted_at DESC");
-while ($row = $result->fetch_assoc()) {
-    echo "<p><strong>{$row['customer_name']}</strong>: {$row['message']} ({$row['submitted_at']})</p>";
-}
-?>
