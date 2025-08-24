16c44a6b09593284d2a5754e6fb8fa70f5056447
shanthana06 <shanthanalaxhimi@gmail.com>
2025-08-24 13:03:02 +0800

    Delete staff_orders.php

--- staff_orders.php
+++
@@ -1,8 +0,0 @@
-<?php include 'db.php'; ?>
-<h2>Orders</h2>
-<?php
-$result = $conn->query("SELECT * FROM orders ORDER BY order_date DESC");
-while ($row = $result->fetch_assoc()) {
-    echo "<p>{$row['customer_name']} ordered {$row['item_name']} ({$row['quantity']}) on {$row['order_date']}</p>";
-}
-?>
