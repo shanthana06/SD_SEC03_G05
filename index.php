685f8041c193ef30695579ac19eba57782b4884c
leneaaale <PEILINGBTS@GMAIL.COM>
2025-09-11 01:12:10 +0800

    Describe what you changed

--- index.php
+++ index.php
@@ -9,7 +9,7 @@
   <meta name="viewport" content="width=device-width, initial-scale=1.0" />
   <title>Arjuna n Co-ffee Ordering System</title>
 
-  <!-- Bootstrap CSS & Icons --><!-- test change for commit -->
+  <!-- Bootstrap CSS & Icons --><!-- test change for commit --->
 
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet" />
   <link href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.5/font/bootstrap-icons.css" rel="stylesheet" />
