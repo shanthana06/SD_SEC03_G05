493dfdd07e3ffd681506ebc8afe80649060ad5ee
leneaaale <PEILINGBTS@GMAIL.COM>
2025-09-22 00:13:22 +0800

    resolve merge conflict

--- generate_hash.php
+++ generate_hash.php
@@ -1,24 +1,8 @@
-<<<<<<< HEAD
 <?php
 echo password_hash("Admin@123", PASSWORD_DEFAULT);
 echo "\n";
 echo password_hash("Staff@123", PASSWORD_DEFAULT);
 echo "\n";
 echo password_hash("Cust@123", PASSWORD_DEFAULT);
+echo "\n";
 ?>
-<?php
-echo password_hash("Admin@123", PASSWORD_DEFAULT);
-echo "\n";
-echo password_hash("Staff@123", PASSWORD_DEFAULT);
-echo "\n";
-echo password_hash("Cust@123", PASSWORD_DEFAULT);
-?>
-=======
-<?php
-echo password_hash("Admin@123", PASSWORD_DEFAULT);
-echo "\n";
-echo password_hash("Staff@123", PASSWORD_DEFAULT);
-echo "\n";
-echo password_hash("Cust@123", PASSWORD_DEFAULT);
-?>
->>>>>>> 9c3cfdedaaf306ac261286e46793cbcf989f68c9
